2746ce70d0e503a47aa58e303150f7f298ed7b10
Nicholas Voss <nicholasvoss02@gmail.com>
2020-05-14 16:05:30 -0600

    Fixed Guns

--- Gun.gd
+++ Gun.gd
@@ -10,7 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var space_state = get_world_2d().direct_space_state
+#	var space_state = get_world_2d().direct_space_state
 	mouse_position = get_local_mouse_position()
 	rotation += mouse_position.angle()
 	var degree = rad2deg($Degree.global_rotation)
@@ -20,18 +20,18 @@ func _process(delta):
 		$Degree/Sprite.flip_v=true
 	else:
 		$Degree/Sprite.flip_v=false
-	if Input.is_action_just_pressed("fire") and $Degree/RayCast2D.is_colliding():	
-#		var result = space_state.intersect_ray($Degree.global_position,$Degree.global_position + transform.x * 1000,[self])
-#		print($Degree/RayCast2D.is_colliding())
-#		print($Degree.global_position)
-#		print(result)
-		$Degree/Beam.set_point_position(0,$Degree.global_position)
-		$Degree/Beam.set_point_position(1,$Degree/RayCast2D.get_collision_point())
-		print($Degree/RayCast2D.get_collision_point())
-	elif Input.is_action_pressed("fire") and $Degree/RayCast2D.is_colliding()==false:
+	if Input.is_action_just_pressed("fire") and $Degree/RayCast2D.is_colliding() == true:	
+		var distance = $Degree.get_global_position().distance_to($Degree/RayCast2D.get_collision_point())
+		print(distance)
 		$Degree/Beam.visible = true
+		$Degree/Beam.add_point(Vector2(distance,0),0)
+		$Degree/Beam.add_point(Vector2(0,0),1)
 		var timer = $Degree/Timer.start()
+#	elif Input.is_action_pressed("fire") and $Degree/RayCast2D.is_colliding()==false:
+#		$Degree/Sprite/Beam.visible = true
+#		var timer = $Degree/Timer.start()
 
 
 func _on_Timer_timeout():
-	$Degree/Beam.visible = true
+	$Degree/Beam.visible = false
+	$Degree/Beam.clear_points()
