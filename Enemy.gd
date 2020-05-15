598e178da82ae381582e94ca87c640abd5785b54
Caleb Rowley <caleb.rowley02@gmail.com>
2020-05-14 22:32:50 -0600

    test room and enemy animation

--- Enemy.gd
+++ Enemy.gd
@@ -1,13 +1,47 @@
 extends Node2D
 
-var health = 2
+var distance
+var velocity = Vector2(0,0)
+var Player 
+var angle
+var direction
+var x
+var y
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	Player = get_node("../Player")
 
+func _physics_process(delta):
+	if abs((Player.position - position).length()) > 2:
+		velocity = (Player.position - position).normalized() * 2
+		angle = velocity.angle()
+		set_animation()
+	else:
+		velocity = Vector2(0,0)
+	position += velocity
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func set_animation():
+	x = cos(angle)
+	y = sin(angle)
+	direction = [
+		abs(y) > abs(x) && y > 0,
+		abs(y) > abs(x) && y < 0,
+		abs(x) > abs(y) && x > 0,
+		abs(x) > abs(y) && x < 0,
+		]
+	match direction:
+		[false, false, false, true]:
+			$Sprite.animation = "Side"
+			$Sprite.flip_h = true
+		[false, false, true, false]:
+			$Sprite.animation = "Side"
+			$Sprite.flip_h = false
+		[false, true, false, false]:
+			$Sprite.animation = "Down"
+			$Sprite.flip_v = true
+		[true, false, false, false]:
+			$Sprite.animation = "Down"
+			$Sprite.flip_v = false
+			
+			
